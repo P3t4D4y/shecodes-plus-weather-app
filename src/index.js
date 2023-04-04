9e3592f7baa1ef228956873624c537a9c7316adb
P3t4D4y <122386374+P3t4D4y@users.noreply.github.com>
2023-04-04 16:27:13 +0100

    JS actual temperature of London

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-
+function displayTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temp-now");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+}
+let apiKey = "f029f4abba90t43ea515f71232oae02a";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
