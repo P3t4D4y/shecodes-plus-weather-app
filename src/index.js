96529aa501489b31139f7d6cb9f8d3ed03fbcee2
P3t4D4y <122386374+P3t4D4y@users.noreply.github.com>
2023-04-05 14:06:32 +0100

    Show current weather for London

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ function displayTemperature(response) {
   console.log(response);
   let temperatureElement = document.querySelector("#temp-now");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#todayDescription");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
+  windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
 }
 let apiKey = "f029f4abba90t43ea515f71232oae02a";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
