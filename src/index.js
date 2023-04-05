9af386665e97ba4e8e75496b4007e055e65faf81
P3t4D4y <122386374+P3t4D4y@users.noreply.github.com>
2023-04-05 16:35:56 +0100

    Connected the search engine

--- src/index.js
+++ src/index.js
@@ -58,8 +58,20 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.condition.description);
 }
-let apiKey = "f029f4abba90t43ea515f71232oae02a";
-let city = "London";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+
+function search(city) {
+  let apiKey = "f029f4abba90t43ea515f71232oae02a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#search-text-input");
+  search(cityInputElement.value);
+}
+
+search("London");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
